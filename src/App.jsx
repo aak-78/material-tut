0d194146f3bbc80bc8f388daef5412bd45ee4f45
aak-78 <a.koltunov1978@gmail.com>
2022-11-26 23:32:09 +0300

    add Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <div>Hello world</div>
+      <div>
+        <Link to={`notes/`}>Notes</Link>
+      </div>
+      <Link to={`create/`}>Create</Link>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import Create from './pages/Create';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'notes/',
+    element: <Notes />,
+  },
+  {
+    path: 'create/',
+    element: <Create />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
